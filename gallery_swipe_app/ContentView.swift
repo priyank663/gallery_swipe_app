4eb5d0c9b5d589a8ffe3e933c908ebc36bc751e9
priyank jaiswal <priyankjaiswal5@gmail.com>
2021-03-22 23:45:14 -0400

    Initial Commit

---
+++ gallery_swipe_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gallery_swipe_app
+//
+//  Created by priyank on 2021-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
