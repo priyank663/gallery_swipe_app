fb2b5e230b161c7f7255d1932d095b5d3f0c6318
priyank jaiswal <priyankjaiswal5@gmail.com>
2021-03-28 23:05:42 -0400

    Added a comment

--- gallery_swipe_app/SceneDelegate.swift
+++ gallery_swipe_app/SceneDelegate.swift
@@ -6,58 +6,172 @@
 //
 
 import UIKit
-import SwiftUI
+import Photos
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate {
 
-    var window: UIWindow?
-
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
+    var myCollectionView: UICollectionView!
+    var imageArray=[UIImage]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        self.title = "Photos"
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView.delegate=self
+        myCollectionView.dataSource=self
+        myCollectionView.register(PhotoItemCell.self, forCellWithReuseIdentifier: "Cell")
+        myCollectionView.backgroundColor=UIColor.secondarySystemBackground
+        self.view.addSubview(myCollectionView)
+        
+        myCollectionView.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue)))
+        
+        grabPhotos()
+    }
+    
+    //MARK: CollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoItemCell
+        cell.img.image=imageArray[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+        let vc=ImagePreviewVC()
+        vc.imgArray = self.imageArray
+        vc.passedContentOffset = indexPath
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        //        if UIDevice.current.orientation.isPortrait {
+        //            return CGSize(width: width/4 - 1, height: width/4 - 1)
+        //        } else {
+        //            return CGSize(width: width/6 - 1, height: width/6 - 1)
+        //        }
+        if DeviceInfo.Orientation.isLandscape {
+            return CGSize(width: width/4 - 1, height: width/4 - 1)
+        } else {
+            return CGSize(width: width/6 - 1, height: width/6 - 1)
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    
+    //MARK: grab photos
+    func grabPhotos(){
+        imageArray = []
+        
+        DispatchQueue.global(qos: .background).async {
+            print("This is run on the background queue")
+            let imgManager=PHImageManager.default()
+            
+            let requestOptions=PHImageRequestOptions()
+            requestOptions.isSynchronous=true
+            requestOptions.deliveryMode = .highQualityFormat
+            
+            let fetchOptions=PHFetchOptions()
+            fetchOptions.sortDescriptors=[NSSortDescriptor(key:"creationDate", ascending: false)]
+            
+            let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            print(fetchResult)
+            print(fetchResult.count)
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count{
+                    imgManager.requestImage(for: fetchResult.object(at: i) as PHAsset, targetSize: CGSize(width:500, height: 500),contentMode: .aspectFill, options: requestOptions, resultHandler: { (image, error) in
+                        self.imageArray.append(image!)
+                    })
+                }
+            } else {
+                print("You got no photos.")
+            }
+            print("imageArray count: \(self.imageArray.count)")
+            
+            DispatchQueue.main.async {
+                print("This is run on the main queue, after the previous code in outer block")
+                self.myCollectionView.reloadData()
+            }
         }
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
 }
 
+
+class PhotoItemCell: UICollectionViewCell {
+    
+    var img = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds=true
+        self.addSubview(img)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        img.frame = self.bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+struct DeviceInfo {
+    struct Orientation {
+        // indicate current device is in the LandScape orientation
+        static var isLandscape: Bool {
+            if #available(iOS 14.4, *) {
+                    return UIApplication.shared.windows
+                        .first?
+                        .windowScene?
+                        .interfaceOrientation
+                        .isLandscape ?? false
+                } else {
+                    return UIApplication.shared.statusBarOrientation.isLandscape
+                }
+            
+            }
+        }
+        // indicate current device is in the Portrait orientation
+    static var isPortrait: Bool {
+            if #available(iOS 13.0, *) {
+                return UIApplication.shared.windows
+                    .first?
+                    .windowScene?
+                    .interfaceOrientation
+                    .isPortrait ?? false
+            } else {
+                return UIApplication.shared.statusBarOrientation.isPortrait
+            }
+        }
+    }
